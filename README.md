606a0d4f9aef0a49d54f416ba0f1389bdac5f6bb
zerocyberlabs <black.tokyo777@gmail.com>
2025-08-01 18:12:58 -0600

    zero

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# prototypo
+
+# Ninastech-United
+
+https://ninastech-united.vercel.app/
+
+![linkedin - Made with Clipchamp](https://github.com/NinasTech/Ninastech-United/assets/82294375/7644915e-a510-4cef-b43d-6c49e11e7fde)
+# zerocyberlabs1
+# zerocyberlabs
+# zerocyberlabs
+# zerocyberlabs
+# zerocyberlabs
---
+++ script.js
@@ -0,0 +1,63 @@
+"use strict";
+
+// ADD EVENT LISTENER ON MULTIPLE ELEMENTS
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+// NAVBAR TOGGLE FOR MOBILE
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+// HEADER - ACTIVE HEADER WHEN SCROLL DOW TO 100px
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+/**
+ * ACCORDION
+ */
+
+const accordions = document.querySelectorAll("[data-accordion]");
+
+let lastActiveAccordion = accordions[0];
+
+const initAccordion = function (currentAccordion) {
+  const accordionBtn = currentAccordion.querySelector("[data-accordion-btn]");
+
+  const expandAccordion = function () {
+    if (lastActiveAccordion && lastActiveAccordion !== currentAccordion) {
+      lastActiveAccordion.classList.remove("expanded");
+    }
+
+    currentAccordion.classList.toggle("expanded");
+
+    lastActiveAccordion = currentAccordion;
+  };
+
+  accordionBtn.addEventListener("click", expandAccordion);
+};
+
+for (let i = 0, len = accordions.length; i < len; i++) {
+  initAccordion(accordions[i]);
+}
